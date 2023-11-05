153316f61ab7b2162c2eee9657462239e7570e40
mahadi-nsu <mahadi.hassan@northsouth.edu>
2023-11-05 08:20:50 +0600

    setup: shadcn ui library added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
+import { Button } from "@/components/ui/button";
+
 function App() {
   
 
   return (
-   <div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
-   </div>
-  )
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App
